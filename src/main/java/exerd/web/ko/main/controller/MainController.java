9029e1cba1276959dadcbdf6f4f19cad7d5e942b
이범선 <lbs@tomatosystem.co.kr>
2021-04-19 08:43:45 +0900

    페이징 작업중

--- src/main/java/exerd/web/ko/main/controller/MainController.java
+++ src/main/java/exerd/web/ko/main/controller/MainController.java
@@ -1,10 +1,19 @@
 package exerd.web.ko.main.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.reflect.TypeToken;
 import exerd.web.ko.main.service.CustomerService;
+import exerd.web.ko.main.vo.CustomerVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class MainController {
 
@@ -13,7 +22,26 @@ public class MainController {
 
     @GetMapping(value="/")
     public String main(Model model) {
-        model.addAttribute("customerList",customerService.getCustomerList());
+        Gson gson = new Gson();
+
+        List<CustomerVO> customerTotalList = new ArrayList<>();
+        customerTotalList = customerService.getCustomerList();
+
+        int customerListCount = 0;
+
+        if(customerTotalList.size() > 0){
+            customerListCount = customerTotalList.size() / 15;
+            if((customerListCount % 15) > 0){
+                customerListCount += 1;
+            }
+        }
+
+        JsonElement element = gson.toJsonTree(customerTotalList, new TypeToken<List<CustomerVO>>() {}.getType());
+        JsonArray jsonArray = element.getAsJsonArray();
+
+        model.addAttribute("customerList", jsonArray);
+        model.addAttribute("customerListCount", customerListCount);
+
         return "index.html";
     }
 }
