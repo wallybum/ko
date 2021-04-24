c7b4dbddfca609e88ba3437386efeb7141c5fc24
이범선 <lbs@tomatosystem.co.kr>
2021-04-24 17:22:48 +0900

    index 페이지 우측 메뉴 조정 및 다운로드 페이지 작성중

---
+++ src/main/java/exerd/web/ko/main/controller/DownloadController.java
@@ -0,0 +1,18 @@
+package exerd.web.ko.main.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DownloadController {
+
+    @Autowired
+//    DownloadService downloadService;
+
+    @GetMapping(value="/download")
+    public String main(){
+
+        return "download.html";
+    }
+}
